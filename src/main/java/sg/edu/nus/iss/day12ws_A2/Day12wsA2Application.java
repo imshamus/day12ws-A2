e77fa97ed47bff7cc1e1c4e5299bc4a15ededd85
imshamus <shamus-sjh@hotmail.com>
2024-11-23 17:46:08 +0800

    added error page

---
+++ src/main/java/sg/edu/nus/iss/day12ws_A2/Day12wsA2Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day12ws_A2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12wsA2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12wsA2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day12ws_A2/Day12wsA2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day12ws_A2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12wsA2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
